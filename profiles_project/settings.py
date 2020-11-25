cb687bd4be5cd94d00ec3b077564861804751d28
mnpenchev <mnpenchev@gmail.com>
2020-11-25 16:41:27 +0000

    SS

--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 import os
 from pathlib import Path
-from . import secret_settings
+# from . import secret_settings
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +21,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = secret_settings.SECRET_KEY
+# SECRET_KEY = secret_settings.SECRET_KEY
+SECRET_KEY = '%#$*'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(int(os.environ.get('DEBUG', 1)))
